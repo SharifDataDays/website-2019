d8d4bf7015821c175b3a1869b0fd2803b2b7107c
garjania <a.garjani@gmail.com>
2019-01-16 00:24:31 +0330

    footer_script changed

---
+++ static/files/js/scroll.js
@@ -0,0 +1,22 @@
+$(document)
+  .scroll(function() {
+	if (location.pathname != "/" && location.pathname != "") {
+		console.log("gg");
+		return;
+	}
+        var navbar = $('#navbar');
+        navbar.css('z-index', '999');
+
+        var scrollY = window.pageYOffset;
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+        if (scrollY >= 15) {
+            navbar.addClass('reverse-navbar-color');
+            navbar.removeClass('navbar-color');
+        }
+        else {
+            navbar.addClass('navbar-color');
+            navbar.removeClass('reverse-navbar-color');
+        }
+
+});
