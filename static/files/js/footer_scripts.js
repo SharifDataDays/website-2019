048aa00fc9b76b2c85c523bcc6eea977a9d34fd9
root <root@fsug.sharif.edu>
2019-01-15 21:22:04 +0330

    Changes to be committed: 	modified:   apps/accounts/locale/fa/LC_MESSAGES/django.po 	modified:   apps/accounts/templates/accounts/profile/signup.html 	modified:   apps/accounts/templates/email/locale/fa/LC_MESSAGES/django.po 	modified:   apps/billing/locale/fa/LC_MESSAGES/django.po 	modified:   apps/game/locale/fa/LC_MESSAGES/django.po 	modified:   apps/intro/locale/fa/LC_MESSAGES/django.po 	modified:   apps/intro/templates/intro/faq.html 	modified:   apps/intro/templates/intro/index_2.html 	new file:   apps/intro/templates/intro/new 	new file:   apps/intro/templates/intro/tempindex 	modified:   locale/fa/LC_MESSAGES/django.po 	deleted:    static/files/assets/time-line.png 	modified:   static/files/css/styles.css 	modified:   static/files/js/footer_scripts.js 	modified:   templates/base/header.html

--- static/files/js/footer_scripts.js
+++ static/files/js/footer_scripts.js
@@ -12,10 +12,20 @@ $(document)
         var navbar = $('#navbar');
         navbar.css('z-index', '999');
 
+	window.scrollBy(0, 50);
         $('time').attr("dir", "ltr")
+	if (location.pathname != "/" && location.pathname != "") {
+		navbar.addClass('reverse-navbar-color');
+		navbar.removeClass('navbar-color');
+		console.log("hh");
+	}
 
     })
-    .scroll(function(){
+    .scroll(function() {
+	if (location.pathname != "/" && location.pathname != "") {
+		console.log("gg");
+		return;
+	}
         var navbar = $('#navbar');
         navbar.css('z-index', '999');
 
