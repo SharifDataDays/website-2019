a9f7a5e4e41d2e3b26e6fb0cdfb6c72f62d7e8e3
root <root@fsug.sharif.edu>
2019-01-16 15:01:50 +0330

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   apps/accounts/locale/fa/LC_MESSAGES/django.po
    	modified:   apps/accounts/templates/accounts/profile/signup.html
    	modified:   apps/accounts/templates/email/locale/fa/LC_MESSAGES/django.po
    	modified:   apps/billing/locale/fa/LC_MESSAGES/django.po
    	modified:   apps/game/locale/fa/LC_MESSAGES/django.po
    	modified:   apps/intro/locale/fa/LC_MESSAGES/django.po
    	modified:   apps/intro/templates/intro/index_2.html
    	modified:   locale/fa/LC_MESSAGES/django.po
    	modified:   static/files/js/footer_scripts.js
    	new file:   static/files/js/scroll.js
    	deleted:    temp
    	modified:   templates/base/base.html

--- static/files/js/footer_scripts.js
+++ static/files/js/footer_scripts.js
@@ -1,7 +1,5 @@
 $('.ui.dropdown').dropdown();
 
-$('#navbar').sticky({context: '#main'});
-
 $('.ui.accordion').accordion();
 
 $('.popup-link').popup();
@@ -20,28 +18,9 @@ $(document)
 		console.log("hh");
 	}
 
-    })
+    });
 
-    .scroll(function() {
-	if (location.pathname != "/" && location.pathname != "") {
-		console.log("gg");
-		return;
-	}
-        var navbar = $('#navbar');
-        navbar.css('z-index', '999');
 
-        var scrollY = window.pageYOffset;
-        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-        if (scrollY >= 15) {
-            navbar.addClass('reverse-navbar-color');
-            navbar.removeClass('navbar-color');
-        }
-        else {
-            navbar.addClass('navbar-color');
-            navbar.removeClass('reverse-navbar-color');
-        }
-
-});
 
 
