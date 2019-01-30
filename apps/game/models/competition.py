b2e092719cade8ce46f4c7b915277d73e397e791
Mjg79 <mahdi.javan.gh2@gmail.com>
2019-01-30 16:51:56 +0330

    file uploading handle

--- apps/game/models/competition.py
+++ apps/game/models/competition.py
@@ -96,6 +96,7 @@ class Choice(models.Model):
 class FileUploadQuestion(Question):
     download_url = models.CharField(max_length=200)
     upload_url = models.CharField(max_length=200)
+    answer_file = models.FileField(upload_to=upload_url,null=True)
 
     def save(self, **kwargs):
         self.type = 'file_upload'
