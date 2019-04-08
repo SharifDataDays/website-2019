3268efc0c276f9a121c2c027a949ab764a94af5b
mrtaalebi <the.doors.are.locked@gmail.com>
2019-04-08 23:56:33 +0430

    allah allah

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 import multiprocessing
 
-bind = "unix:/home/datadays/website-2019/aic_site/gunicorn.sock"
+bind = "unix:/home/ssc/sites/datadays/aic_site/gunicorn.sock"
 workers = 4
-errorlog = "/home/datadays/logs/datadays_gunicorn.log"
+errorlog = "/home/ssc/logs/datadays_gunicorn.log"
 loglevel = "debug"
 proc_name = "datadays"
--- gunicorn_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export DJANGO_SETTINGS_MODULE='aic_site.settings.development'
-
-exec /home/datadays/env/bin/gunicorn -c gunicorn.conf.py aic_site.wsgi:application
