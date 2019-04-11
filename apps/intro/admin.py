da8cc21827d2102b13352751c548a79c862c92b0
arghavan99 <a.rezvani99@yahoo.com>
2019-04-11 23:31:09 +0430

    -_____-

--- apps/intro/admin.py
+++ apps/intro/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
 
-#from apps.intro.models import StaffReborn, StaffTeam, StaffSubTeam
-#
-#
-#class StaffRebornAdmin(ModelAdmin):
-#    class Meta:
-#        model = StaffReborn
-#        fields = [
-#                'name',
-#                'sub_team',
-#                'image'
-#                ]
-#
-#admin.site.register(StaffReborn, StaffRebornAdmin)
+from apps.intro.models import StaffReborn, StaffTeam, StaffSubTeam, Staff
+
+
+class StaffRebornAdmin(admin.ModelAdmin):
+    fields = [
+               'name',
+               'sub_team',
+               'image'
+               ]
+
+
+class StafAdmin(admin.ModelAdmin):
+    fields = ['name', 'team', 'image']
+
+
+class StafTeamAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
+class StafSubTeamAdmin(admin.ModelAdmin):
+    fields = ['name', 'parent_team']
+
+
+admin.site.register(Staff, StafAdmin)
+admin.site.register(StaffSubTeam, StafSubTeamAdmin)
+admin.site.register(StaffTeam, StafTeamAdmin)
+admin.site.register(StaffReborn, StaffRebornAdmin)
