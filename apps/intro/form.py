a3fc5fc31ef9b05b90856d34297497136c2791ce
garjania <a.garjani@gmail.com>
2019-04-12 15:37:22 +0430

    khoshgelized staff

--- apps/intro/form.py
+++ apps/intro/form.py
@@ -8,7 +8,7 @@ class StaffForm(forms.Form):
 
     name = forms.CharField(max_length=128, required=True)
 
-    team = forms.ChoiceField(choices=([(sub_team.__str__(), sub_team.__str__()) for sub_team in StaffSubTeam.objects.all()]),
-            required=True)
+    # team = forms.ChoiceField(choices=([(sub_team.__str__(), sub_team.__str__()) for sub_team in StaffSubTeam.objects.all()]),
+    #         required=True)
 
     image = forms.FileField(required=True)
